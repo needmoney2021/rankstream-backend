f765c9df730c80b143e0eaf57f6d8f4d23867448
needmoney2021 <limemimorinn@gmail.com>
2025-04-09 16:39:38 +0900

    도메인 별 서비스 클래스 생성

---
+++ domain/src/main/kotlin/com/rankstream/backend/domain/grade/service/GradeService.kt
@@ -0,0 +1,13 @@
+package com.rankstream.backend.domain.grade.service
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Service
+
+@Service
+class GradeService {
+
+    companion object {
+        private val log: Logger = LoggerFactory.getLogger(GradeService::class.java)
+    }
+} 
---
+++ domain/src/main/kotlin/com/rankstream/backend/domain/schedule/service/ScheduleService.kt
@@ -0,0 +1,13 @@
+package com.rankstream.backend.domain.schedule.service
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Service
+
+@Service
+class ScheduleService {
+
+    companion object {
+        private val log: Logger = LoggerFactory.getLogger(ScheduleService::class.java)
+    }
+} 
